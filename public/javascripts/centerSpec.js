7dd0aff04ec2ff109d78bc6fb93cacbb50819186
tophat8855 <hrfarley@gmail.com>
2015-02-18 09:35:51 -0800

    centers a long string

--- public/javascripts/centerSpec.js
+++ public/javascripts/centerSpec.js
@@ -6,4 +6,12 @@ describe('center string', function () {
 
     expect(result).toEqual('                              this is a short line                              ');
   });
+
+  it ('centers a long string', function() {
+    var string = 'this is a really long line that has a lot of characters';
+
+    var result = center(string);
+
+    expect(result).toEqual('            this is a really long line that has a lot of characters             ');
+  });
 });
